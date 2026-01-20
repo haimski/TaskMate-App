980d0c3bb2da391c51e9e4db187095484041a3e3
haimski <benzak@gmail.com>
2026-01-20 12:40:51 +0200

    readable Date

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import {type Task } from './task.model';
 import { CardComponent } from "../../common/card/card.component";
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, DatePipe],
   templateUrl: './task.component.html',
   styleUrls: ['./task.component.css']
 })
